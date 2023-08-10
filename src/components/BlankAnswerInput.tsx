9ff56b6f126c45852ead5970293e19c8efac852f
swastik <swastikpatel29@gmail.com>
2023-08-10 19:48:38 +0530

    extracted main keywords from Ans & show inputboxes

--- src/components/BlankAnswerInput.tsx
+++ src/components/BlankAnswerInput.tsx
@@ -1,13 +1,58 @@
-import React from 'react'
+import React from 'react';
+import { useMemo } from 'react';
+import keyword_extractor from 'keyword-extractor';
 
 type Props = {
     answer: string;
 }
 
+const BLANKS = '_____';
+
 const BlankAnswerInput = ({answer}: Props) => {
+
+    //might be quite expensive so we don't want to recalculate the keyword of a particular question everytime the component is rendered , so we will use useMemo 
+    //useMemo will only re-render/recalculate the keyword when the answer changes 
+
+    const keywords = useMemo(() => {
+        const words = keyword_extractor.extract(answer, {
+            language: "english",
+            remove_digits: true,
+            return_changed_case: false,
+            remove_duplicates: false,
+        });
+        const shuffled = words.sort(() => Math.random() - 0.5);         //applied the same technique to shuffle the options in mcq question
+        return shuffled.slice(0, 2);                                    //will get 2 main random keywords from the answer
+
+    }, [answer]);
+
+
+    const answerWithBlanks = React.useMemo(() => {
+        const answerWithBlanks = keywords.reduce((acc, keyword) => {
+          return acc.replaceAll(keyword, BLANKS);
+        }, answer);
+        // setBlankAnswer(answerWithBlanks);
+        return answerWithBlanks;
+      }, [answer, keywords,]);
+
+
   return (
     <div className=' flex justify-start w-full mt-4'>
-        <h1 className=' text-xl font-semibold'>{answer}</h1>
+        <h1 className=' text-xl font-semibold'>
+            {answerWithBlanks.split(BLANKS).map((part, index) => {
+                return(
+                    <>
+                    {part}
+                    {index === answerWithBlanks.split(BLANKS).length - 1 ? null : (     //doing this to remove the 'byDefault' blank at the end of the answer
+                        <input
+                        id='user-blank-input'
+                        className="text-center border-b-2 border-black dark:border-white w-28 focus:border-2 focus:border-b-4 focus:outline-none"
+                        type="text"  
+                        />
+                    )}
+                    </>
+                )
+            })}
+        </h1>
     </div>
   )
 }
